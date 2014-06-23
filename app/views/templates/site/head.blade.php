8a058d8e5c50f629a0f23c7a1f76a10b0450062a
Artem Tumin <arttumin@gmail.com>
2014-06-23 18:41:17 +0400

    favicon

--- app/views/templates/site/head.blade.php
+++ app/views/templates/site/head.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+        <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
 
         {{ HTML::style('css/fonts.css') }}
         {{ HTML::style('css/normalize.css') }}
